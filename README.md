b583555cee2f5aa1a5b845d2e4ff515cbc79bcd4
Kusudama <kusudama.kus@mail.ru>
2023-06-11 20:45:37 +0300

    test2

---
+++ README.md
@@ -0,0 +1,11 @@
+# Задание “Geometry API”
+
+# Техническое задание:
+Разработать CRUD REST Web API для работы с геометрией.
+
+## Используемые технологии:
+- Node.js
+- Express
+- PostgreSQL 14
+- Typescript
+- Pino
--- queries.js
+++ queries.js
@@ -7,14 +7,14 @@ const pool = new Pool({
     port: 5432,
 })
 const getUsers = (request, response) => {
-        pool.query('SELECT * FROM geometry ORDER BY id ASC', (error, results) => {
-            if (error) {
-                throw error
-            }
-            response.status(200).json(results.rows)
-        })
-    }
-    //test
+    pool.query('SELECT * FROM geometry ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
 const getUserById = (request, response) => {
     const id = parseInt(request.params.id)
 
