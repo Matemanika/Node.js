d00606159b401830f08c8aab0911cd159349a881
Kusudama <kusudama.kus@mail.ru>
2023-06-11 19:01:04 +0300

    create new project Node.js, Express, and Postgres API

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/geometry', db.getUsers)
+app.get('/geometry/:id', db.getUserById)
+app.post('/geometry', db.createUser)
+app.put('/geometry/:id', db.updateUser)
+app.delete('/geometry/:id', db.deleteUser)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,72 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'math',
+    password: '1111',
+    port: 5432,
+})
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM geometry ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * FROM geometry WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createUser = (request, response) => {
+    const { picture, authors } = request.body
+
+    pool.query('INSERT INTO geometry (picture, authors) VALUES ($1, $2)', [picture, authors], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${results.insertId}`)
+    })
+}
+
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { picture, authors } = request.body
+
+    pool.query(
+        'UPDATE geometry SET picture = $1, authors = $2 WHERE id = $3', [picture, authors, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified with ID: ${id}`)
+        }
+    )
+}
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM geometry WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User deleted with ID: ${id}`)
+    })
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+}
